b1a734577153ceded4db759e4da0a2de03cce140
Sébastien <sebastien.levesque.slev@gmail.com>
2023-05-05 02:19:19 -0400

    h

--- src/components/video-auto-pause.js
+++ src/components/video-auto-pause.js
@@ -6,6 +6,7 @@ addEventListener('load', init);
 function init()
 {
     loadVideosMetaData();
+    setVideoStatus();
 }
 
 function loadVideosMetaData()
@@ -15,15 +16,22 @@ function loadVideosMetaData()
     }
 }
 
+/*setInterval(setVideoStatus, 5000);*/
+
 window.addEventListener("scroll", function(event) {
-	for (let i = 0; i < videos.length; i++) {
+	setVideoStatus();
+});
+
+function setVideoStatus(){
+    for (let i = 0; i < videos.length; i++) {
+
 		const video = videos[i];
 		const videoRect = video.getBoundingClientRect();
 		if (videoRect.bottom <= 0 || videoRect.top >= windowHeight) {
             if (video.paused) { return; }
-			video.pause();
+            video.pause();
 		} else if (video.paused){
 			video.play();
 		}
 	}
-});
+}
