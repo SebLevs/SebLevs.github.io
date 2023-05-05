720dde8bab4ba095eadb086f2e16d08ce085d347
Sébastien <sebastien.levesque.slev@gmail.com>
2023-05-05 01:53:23 -0400

    fix: prevent video freeze upon reaching its end

--- src/components/video-auto-pause.js
+++ src/components/video-auto-pause.js
@@ -1,9 +1,22 @@
 const videos = document.getElementsByTagName("video");
 const windowHeight = window.innerHeight || document.documentElement.clientHeight;
 
+addEventListener('load', init);
+
+function init()
+{
+    loadVideosMetaData();
+}
+
+function loadVideosMetaData()
+{
+    for (let i = 0; i < videos.length; i++) {
+        videos[i].load;
+    }
+}
+
 window.addEventListener("scroll", function(event) {
 	for (let i = 0; i < videos.length; i++) {
-        
 		const video = videos[i];
 		const videoRect = video.getBoundingClientRect();
 		if (videoRect.bottom <= 0 || videoRect.top >= windowHeight) {
